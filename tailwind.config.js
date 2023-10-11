1f30aba1838cdd833dfa87528add36b4b5b3e7f0
Akabim <abimanyu.ari.d.123@gmail.com>
2023-10-11 21:46:40 +0700

    landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,6 @@ module.exports = {
   content: ["index.html"],
   theme: {
     extend: {
-      fontFamily: {
-        custom: ["Helvetica", "sans"],
-      },
       colors: {
         primary: "#8566FF",
         secondary: "#212121",
