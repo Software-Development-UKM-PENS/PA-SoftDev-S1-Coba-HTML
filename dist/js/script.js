3345bc4588b4da92ce4751040e64f4a70f44ae17
Akabim <abimanyu.ari.d.123@gmail.com>
2023-10-13 14:30:47 +0700

    sidebar

--- dist/js/script.js
+++ dist/js/script.js
@@ -6,14 +6,6 @@ hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
 });
-
-const sb = document.querySelector("#search-button");
-
-document.addEventListener("click", function (e) {
-  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
-    searchForm.classList.remove("active");
-  }
-});
 window.onscroll = function () {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
@@ -26,3 +18,38 @@ window.onscroll = function () {
 };
 
 feather.replace();
+
+// const instanceMode = te.Sidenav.getInstance(
+//   document.getElementById("sidenav-2")
+// );
+// const modes = ["push", "over", "side"];
+
+// modes.forEach((mode) => {
+//   const modeSwitch = document.getElementById(mode);
+//   modeSwitch.addEventListener("click", () => {
+//     const instance = te.Sidenav.getInstance(
+//       document.getElementById("sidenav-2")
+//     );
+//     instance.changeMode(mode);
+//     modes.forEach((el) => {
+//       if (el === mode) {
+//         ["text-primary-600", "border-primary-600"].forEach((item) =>
+//           modeSwitch.classList.remove(item)
+//         );
+//         modeSwitch.className +=
+//           " bg-primary text-white hover:bg-primary-700 active:bg-primary-800 focus:bg-primary-700 border-transparent";
+//       } else {
+//         const node = document.getElementById(el);
+//         node.className += " text-primary-600 border-primary-600";
+//         [
+//           "bg-primary",
+//           "text-white",
+//           "hover:bg-primary-700",
+//           "active:bg-primary-800",
+//           "focus:bg-primary-700",
+//           "border-transparent",
+//         ].forEach((item) => node.classList.remove(item));
+//       }
+//     });
+//   });
+// });
